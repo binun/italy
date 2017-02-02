3731c6ada4943fe2cd12ab4317bfcb013cef78e9
Alexander Binun <binun@cs.bgu.ac.il>
2017-02-02 21:23:04 +0200

    w

--- docker/createImages.sh
+++ docker/createImages.sh
@@ -19,12 +19,12 @@ cd ..
 docker build -t "image-cassandra" cassandra
 cd ..
 
-#cd mysql && docker build -t "image-mysql" . && cd ..
+cd mysql && docker build -t "image-mysql" . && cd ..
 
-#cd mariadb && docker build -t "image-mariadb" . && cd ..
+cd mariadb && docker build -t "image-mariadb" . && cd ..
 
-#cd mongodb && docker build -t "image-mongodb" . && cd ..
+cd mongodb && docker build -t "image-mongodb" . && cd ..
 
-#cd cassandr && docker build -t "image-cassandra" cassandra && cd ..
+cd cassandr && docker build -t "image-cassandra" cassandra && cd ..
 
-#cd java8 && docker build -t "image-java" . && cd ..
+cd java8 && docker build -t "image-java" . && cd ..
